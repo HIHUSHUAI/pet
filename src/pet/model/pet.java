5b3caadeeb490e12247ea126877d686464f4e7a7
hihushuai <hihushuai@gmail.com>
2020-02-11 16:25:04 +0800

    基本窗口及功能实现。

---
+++ src/pet/model/pet.java
@@ -0,0 +1,8 @@
+package pet.model;
+
+public abstract class pet {
+	private String pet_name;
+	private String pet_nickname;
+	private int action_number;
+
+}
---
+++ src/pet/program/start.java
@@ -0,0 +1,15 @@
+package pet.program;
+
+
+public class start extends window{
+
+	public static void main(String[] args) {
+		start();
+	}
+
+
+	public static void start() {
+		jframe();
+		setTray();
+	}
+}
